ac93ddc30cd473aea71677e875f67dc80ba0f364
root <root@Onmazir.localdomain>
2024-08-18 21:43:05 +0100

    Sum of Difference of two Numbers

---
+++ sumOfDiff.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+	
+    int a;
+    int b;
+    scanf("%d %d", a b);
+    
+    float c;
+    float g;
+    scanf("%f %f", c g);
+    
+    int sum = 
+    
+    
+    return 0;
+}
